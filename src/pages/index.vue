987745c112317cc5560980303b8e8297b842fe79
zhangnn <1543824790@qq.com>
2023-10-26 22:48:55 +0800

    添加 auto-import

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,11 @@
 <template>
   <div>Hello World</div>
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div>{{ msg }}</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const msg = ref('hello world')
+</script>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 import VueRouter from 'unplugin-vue-router/vite'
 import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,7 +17,25 @@ export default defineConfig({
     }),
     vue(),
     vueJsx(),
-    UnoCSS()
+    UnoCSS(),
+    AutoImport({
+      /* options */
+
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        VueRouterAutoImports
+      ]
+    })
   ],
   resolve: {
     alias: {
